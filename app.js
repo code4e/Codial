daeeb9d66af1d5464195c256fb3798a677426adf
Umar Faiq <umar.sid007@gmail.com>
2020-07-21 21:26:49 +0530

    set up layouts and static files

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const port = 8000;
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/assets'));
 
+//extract styles and scripts from the sub pages to the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,13 +1,9 @@
 module.exports.profile = function(req, res){
     return res.render('users', {
         title: 'users'
-    })
-}
-module.exports.posts = function(req, res){
-    return res.render('users', {
-        title: 'users'
     });
 }
+
 // module.exports.username = function(req, res){
 //     return res.send('<h1>Username Rendered</h1>');
 // }
